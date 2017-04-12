cb156bc9944ce2b1b1138821559704d575d0312f
kun wan <779483309@qq.com>
2017-04-12 11:08:18 +0800

    commit

--- chapter7/main.py
+++ chapter7/main.py
@@ -57,5 +57,79 @@ print dict4.items()
 # 重复值不会报错，但是会覆盖
 print {'z': 1, 'z': 2}
 
+# 集合只能用工厂方法set（）创建
+s = set('nimashsh')
+print s
+s.add('sss')
+print s
+s.update('hello')
+print s
+s.remove('a')
+print s
+s -= set('sss')
+print s
 
+s = set((1, 2, 3))
+print s
+s2 = set()
+s2.add(1)
+s2.add(2)
+s2.add(3)
+print s2 == s
+print s2, s
+
+# 子集与超集
+s2.add(4)
+print s < s2
+print set('shop') < set('cheeseshop')
+print set('bookshop') >= set('shop')
+
+# 交 并 补 异或
+s1 = set('abc')
+s2 = set('acdef')
+print s1 | s2
+print s1 & s2
+print s2 - s1
+print s1 ^ s2
+
+# 不同类型的操作数，操作结果与左侧相同
+s1 = frozenset('abc')
+s2 = set('acdef')
+print s1 | s2
+print s1 & s2
+print s1 ^ s2
+print '*' * 100
+print s2 | s1
+print s2 & s1
+print s2 ^ s1
+
+# 4个仅适用于可变集合的操作符
+print '*' * 100
+s1 = set('abc')
+s1 |= set('def')
+print s1
+
+print '*' * 100
+s1 = set('abc')
+s1 &= set('acedf')
+print s1
+
+print '*' * 100
+s1 = set('abc')
+s1 ^= set('acedf')
+print s1
+
+print '*' * 100
+s1 = set('abc')
+s1 -= set('acedf')
+print s1
+
+# set（）参数必须是可迭代的，即一个序列， 迭代器， 或支持迭代的一个对象， 一个文件 或 一个字典
+ddict = {'a': 1, 'b': 2}
+print set(ddict)
+
+f = open('data.in', 'r')
+print set(f)
+
+# 操作符与内建函数：操作符两边必须是集合， 内建方法对象可以是可迭代的
 
